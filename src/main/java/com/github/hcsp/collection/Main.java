97e9b5f137c0c93b60b6e4f1db743656eeabad72
Maricaya <43136257+Maricaya@users.noreply.github.com>
2019-12-17 19:01:29 +0800

    feat: 将List处理为Map<String, List> (#66)
    feat: 将List处理为Map<String, List> (#66)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +10,27 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Map<String, List<User>> result = new HashMap<>();
+        for (User user:users) {
+            if (result.containsKey(user.getDepartment()) == false) {
+                result.put(user.getDepartment(), new ArrayList<>());
+            }
+            result.get(user.getDepartment()).add(user);
+        }
+        // 排序
+        for (List<User> user_list: result.values()) {
+            Collections.sort(user_list, (o1, o2) -> {
+                if (o1.getAge() > o2.getAge()) {
+                    return 1;
+                } else if (o1.getAge() < o2.getAge()) {
+                    return -1;
+                }
+                return 0;
+            });
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
         System.out.println(
