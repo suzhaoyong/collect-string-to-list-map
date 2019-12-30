5de0e54d47c47b70242cd0ffad296b6ceb87b773
frank-0908 <52615871+frank-0908@users.noreply.github.com>
2019-12-30 16:46:36 +0800

    -[]将List处理成Map <String，List> (#71)
    -[]将List处理成Map <String，List> (#71)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +10,30 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Map<String, List<User>> mapping = new HashMap<>();
+        for (User user : users) {
+            if (!mapping.containsKey(user.getDepartment())) {
+                mapping.put(user.getDepartment(), new ArrayList<>());
+            }
+            mapping.get(user.getDepartment()).add(user);
+        }
+        for (List<User> value : mapping.values()) {
+            Collections.sort(value, new Comparator<User>() {
+                @Override
+                public int compare(User u1, User u2) {
+                    int diff = u1.getAge() - u2.getAge();
+                    if (diff > 0) {
+                        return 1;
+                    } else if (diff < 0) {
+                        return -1;
+                    }
+                    return 0; //相等为0
+                }
+            }); // 按年龄排序
+        }
+        return mapping;
+    }
 
     public static void main(String[] args) {
         System.out.println(
