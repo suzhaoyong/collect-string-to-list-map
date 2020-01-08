8734b0c04a20927663db3b2e3f389b387534ec79
Ir <1319138081@qq.com>
2020-01-08 14:55:22 +0800

    collect-string-to-list completed (#78)
    collect-string-to-list completed (#78)

--- src/main/java/com/github/hcsp/collection/User.java
+++ src/main/java/com/github/hcsp/collection/User.java
@@ -51,4 +51,9 @@ public class User {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+    @Override
+    public String toString() {
+        return id + name + age + department;
+    }
 }
