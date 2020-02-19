c7109dfcb34ee9bce0ef6fd175e4a12d799585d5
TangsongyuanmingQingHad <49867878+TangsongyuanmingQingHad@users.noreply.github.com>
2020-02-18 22:23:10 -0800

    注意最后格式化规范代码！！！ (#101)
    注意最后格式化规范代码！！！ (#101)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +10,25 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Map<String, List<User>> map = new HashMap<String, List<User>>();
+        Set<String> keys = map.keySet();
+        for (User u : users) {
+            String department = u.getDepartment();
+            if (!keys.contains(department)) {
+                List<User> list = new ArrayList<>();
+                list.add(u);
+                map.put(department, list);
+            } else {
+                List<User> users1 = map.get(department);
+                users1.add(u);
+            }
+        }
+        for (String key : map.keySet()) {
+            Collections.sort(map.get(key));
+        }
+        return map;
+    }
 
     public static void main(String[] args) {
         System.out.println(
