d9ecf5fa37d29319a1f203636e28bdeea47b403e
yueyue chou <13444339+yueyuezone@users.noreply.github.com>
2020-02-20 14:05:08 +0800

    修改map (#102)
    修改map (#102)

--- src/main/java/com/github/hcsp/collection/User.java
+++ src/main/java/com/github/hcsp/collection/User.java
@@ -2,7 +2,7 @@ package com.github.hcsp.collection;
 
 import java.util.Objects;
 
-public class User {
+public class User implements Comparable<User> {
     // 用户的id
     private final Integer id;
     // 用户的姓名
@@ -50,5 +50,21 @@ public class User {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+
+    @Override
+    public int compareTo(User o) {
+        return Integer.compare(age, o.age);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", department='" + department + '\'' +
+                '}';
     }
 }
