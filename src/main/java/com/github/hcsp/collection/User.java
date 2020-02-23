ca7bb3244f8a54efb6655a1ed2d1c523697896b7
bonnie1115 <49628449+bonnie1115@users.noreply.github.com>
2020-02-23 22:26:51 +0800

    List处理成Map<String, List> (#103)
    List处理成Map<String, List> (#103)

--- src/main/java/com/github/hcsp/collection/User.java
+++ src/main/java/com/github/hcsp/collection/User.java
@@ -2,7 +2,7 @@ package com.github.hcsp.collection;
 
 import java.util.Objects;
 
-public class User {
+public class User implements Comparable<User> {
     // 用户的id
     private final Integer id;
     // 用户的姓名
@@ -51,4 +51,10 @@ public class User {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+    @Override
+    public int compareTo(User user) {
+        return Integer.compare(this.age, user.age);
+    }
 }
+
