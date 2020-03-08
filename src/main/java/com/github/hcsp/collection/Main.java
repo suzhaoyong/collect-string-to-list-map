610fc52be7fa41db912f718e5d5e0021cfbe8e5e
ARKbreeze <61083620+ARKbreeze@users.noreply.github.com>
2020-03-08 21:14:57 +0800

    修改了格式问题 (#109)
    修改了格式问题 (#109)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,13 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
+
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +17,29 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+
+        HashSet<String> depNameSet = new HashSet();
+        HashMap<String, List<User>> resultMap = new HashMap<>();
+        // 先筛选部门作为map的key
+        for (User user : users) {
+            depNameSet.add(user.getDepartment());
+        }
+
+        //对users排序
+        Collections.sort(users);
+        // 分别对每个部门遍历一遍users
+        for (String depName : depNameSet) {
+            List<User> list = new ArrayList<>();
+            for (User user : users) {
+                if (user.getDepartment() == depName) {
+                    list.add(user);
+                }
+                resultMap.put(depName, list);
+            }
+        }
+        return resultMap;
+    }
 
     public static void main(String[] args) {
         System.out.println(
