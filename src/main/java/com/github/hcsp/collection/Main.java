74324585b5ab33c599d89865671bb6780a4b08b1
huiquan99 <53985556+huiquan99@users.noreply.github.com>
2020-03-09 19:45:50 +0800

    映射 (#111)
    映射 (#111)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +10,36 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+
+    public static Map<String, List<User>> collect(List<User> users) {
+        Set<String> departmentSet = new HashSet<>();
+        for (User user : users) {
+            departmentSet.add(user.getDepartment());
+        }
+
+        Map<String, List<User>> map = new HashMap();
+
+        for (String s : departmentSet) {
+            ArrayList<User> list = new ArrayList<>();
+            for (User user : users) {
+                if (user.getDepartment().equals(s)) {
+                    list.add(user);
+                }
+            }
+
+            list.sort(new Comparator<User>() {
+                @Override
+                public int compare(User o1, User o2) {
+                    return o1.getAge() - o2.getAge();
+                }
+            });
+
+            map.put(s, list);
+
+        }
+
+        return map;
+    }
 
     public static void main(String[] args) {
         System.out.println(
@@ -22,4 +49,5 @@ public class Main {
                                 new User(2, "李四", 30, "技术部"),
                                 new User(3, "王五", 40, "市场部"))));
     }
+
 }
