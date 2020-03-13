103336f5e5640d844c4924caec83f9b571633f4d
walkerxin <44486634+walkerxin@users.noreply.github.com>
2020-03-13 11:10:37 +0800

    将List按age排序，并转为Map<String, List> (#113)
    将List按age排序，并转为Map<String, List> (#113)

--- src/main/java/com/github/hcsp/collection/User.java
+++ src/main/java/com/github/hcsp/collection/User.java
@@ -2,7 +2,7 @@ package com.github.hcsp.collection;
 
 import java.util.Objects;
 
-public class User {
+public class User implements Comparable<User> {
     // 用户的id
     private final Integer id;
     // 用户的姓名
@@ -51,4 +51,15 @@ public class User {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+    @Override
+    public String toString() {
+        return "{name=" + getName() + ", department=" + getDepartment() + ", age=" + getAge() + " }";
+    }
+
+    @Override
+    public int compareTo(User user) {
+        return Integer.compare(getAge(), user.getAge());
+    }
+
 }
