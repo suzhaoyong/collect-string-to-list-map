e726f91066823766104f91aee1919d2ce02ad9e7
ShanQing Huang <58178504+7952126@users.noreply.github.com>
2020-04-25 21:57:12 +0800

    我不会告诉别人我一道题做了两天的 (#127)
    我不会告诉别人我一道题做了两天的 (#127)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,10 +1,9 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
+
     // 请编写一个方法，对传入的List<User>进行如下处理：
     // 返回一个从部门名到这个部门的所有用户的映射。同一个部门的用户按照年龄进行从小到大排序。
     // 例如，传入的users是[{name=张三, department=技术部, age=40 }, {name=李四, department=技术部, age=30 },
@@ -12,8 +11,23 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
 
+    public static Map<String, List<User>> collect(List<User> users) {
+        Map<String, List<User>> NewMapList = new HashMap<>();
+        Collections.sort(users);
+        for (User user : users) {
+            String department = user.getDepartment();
+            List<User> NewUser;
+            if (NewMapList.get(department) != null) {
+                NewUser = NewMapList.get(department);
+            } else {
+                NewUser = new ArrayList<>();
+            }
+            NewUser.add(user);
+            NewMapList.put(department, NewUser);
+        }
+        return NewMapList;
+    }
     public static void main(String[] args) {
         System.out.println(
                 collect(
@@ -23,3 +37,5 @@ public class Main {
                                 new User(3, "王五", 40, "市场部"))));
     }
 }
+
+
