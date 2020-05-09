905391dddebbf5a1f562bd07bae51abae2a75870
YUCHEN LIU <181484908@QQ.COM>
2020-05-09 11:25:11 +0800

    work: list -map (#135)
    work: list -map (#135)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +10,26 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Map<String, TreeSet<User>> map = new HashMap<>();
+        for (User user : users) {
+            String department = user.getDepartment();
+            if (map.containsKey(department)) {
+                TreeSet<User> list = map.get(department);
+                list.add(user);
+            } else {
+                TreeSet<User> list = new TreeSet<>(Comparator.comparingInt(User::getAge));
+                list.add(user);
+                map.put(department, list);
+            }
+        }
+        Map<String, List<User>> result = new HashMap<>();
+        map.forEach((key, value) -> {
+            List<User> users1 = new ArrayList<>(value);
+            result.put(key, users1);
+        });
+        return result;
+    }
 
     public static void main(String[] args) {
         System.out.println(
