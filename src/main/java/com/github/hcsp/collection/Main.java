7cc2977893a1c7f01f7c49c3695430f4c566537e
DavidWildeTMP <64569468+DavidWildeTMP@users.noreply.github.com>
2020-05-11 13:15:31 -0700

    将List处理成Map<String, List> (#137)
    将List处理成Map<String, List> (#137)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +10,24 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Map<String, List<User>> userDepartmentMap = new HashMap<>();
+        for (User user : users) {
+            String userDep = user.getDepartment();
+            if (userDepartmentMap.get(userDep) == null) {
+                List<User> userTemp = new ArrayList<>();
+                userTemp.add(user);
+                userDepartmentMap.put(userDep, userTemp);
+            } else {
+                userDepartmentMap.get(userDep).add(user);
+            }
+        }
+        for (Map.Entry<String, List<User>> entry : userDepartmentMap.entrySet()) {
+            List<User> list = entry.getValue();
+            list.sort(Comparator.comparingInt(User::getAge));
+        }
+        return userDepartmentMap;
+    }
 
     public static void main(String[] args) {
         System.out.println(
