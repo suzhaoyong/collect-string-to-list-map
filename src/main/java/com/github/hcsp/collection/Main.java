e684ae56d481124b8eb9190e0a95b82b719d9562
BenHao Huang <33121651+Lazzben@users.noreply.github.com>
2020-06-08 12:05:08 +0800

    将List处理成Map<String, List> (#145)
    将List处理成Map<String, List> (#145)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +10,21 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Map<String, List<User>> sortedMap = new HashMap<>();
+        Collections.sort(users);
+        for (User user : users) {
+            List<User> departmentUsers;
+            if (sortedMap.get(user.getDepartment()) == null) {
+                departmentUsers = new ArrayList<>();
+            } else {
+                departmentUsers = sortedMap.get(user.getDepartment());
+            }
+            departmentUsers.add(user);
+            sortedMap.put(user.getDepartment(), departmentUsers);
+        }
+        return sortedMap;
+    }
 
     public static void main(String[] args) {
         System.out.println(
