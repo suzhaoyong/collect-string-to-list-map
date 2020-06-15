b6d1dfa156a14394845167f866b6de9e39edf222
daimon0809 <51164058+daimon0809@users.noreply.github.com>
2020-06-15 22:09:30 +0800

    Collection: 将List处理成Map<String, List> (#147)
    Collection: 将List处理成Map<String, List> (#147)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,13 @@
 package com.github.hcsp.collection;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +17,25 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Map<String, List<User>> userMap = new HashMap<>();
+        Set<String> departmentSets = new HashSet<>();
+        for (User user : users) {
+            departmentSets.add(user.getDepartment());
+        }
+
+        for (String department : departmentSets) {
+            List<User> listUser = new ArrayList<>();
+            for (User user : users) {
+                if (department.equals(user.getDepartment())) {
+                    listUser.add(user);
+                }
+            }
+            Collections.sort(listUser);
+            userMap.put(department, listUser);
+        }
+        return userMap;
+    }
 
     public static void main(String[] args) {
         System.out.println(
