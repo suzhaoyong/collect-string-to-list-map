ee815f744d129226de4c2978047f534299d2eb1d
陈晓拉尼 <2524729719@qq.com>
2020-06-25 21:55:33 +0800

    将List处理成Map<String, List> (#151)
    将List处理成Map<String, List> (#151)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,7 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +11,25 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Map<String, List<User>> departmentMap = new HashMap<>();
+
+        for (User user : users) {
+            if (departmentMap.get(user.getDepartment()) == null) {
+                List<User> departTemp = new ArrayList();
+                departTemp.add(user);
+                departmentMap.put(user.getDepartment(), departTemp);
+            } else {
+                departmentMap.get(user.getDepartment()).add(user);
+            }
+        }
+
+        for (List<User> list : departmentMap.values()) {
+            list.sort(Comparator.comparingInt(User::getAge));
+        }
+
+        return departmentMap;
+    }
 
     public static void main(String[] args) {
         System.out.println(
