ea3f457c25fa1ede830ad9524bb10ba580dd7590
ZEJIA LIU <53506531+ZEJIA-LIU@users.noreply.github.com>
2020-07-21 18:08:47 +0800

    将List处理成Map<String, List> (#158)
    将List处理成Map<String, List> (#158)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,9 @@
 package com.github.hcsp.collection;
-
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Arrays;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +13,22 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        sortUsersAsAge(users);
+        Map<String, List<User>> departmentSet = new HashMap<>();
+        for (User user : users) {
+            if (!departmentSet.containsKey(user.getDepartment())) {
+                String newDepartment = user.getDepartment();
+                departmentSet.put(newDepartment, new ArrayList<>());
+            }
+            departmentSet.get(user.getDepartment()).add(user);
+        }
+        return departmentSet;
+    }
+
+    private static void sortUsersAsAge(List<User> users) {
+        users.sort(User::compareTo);
+    }
 
     public static void main(String[] args) {
         System.out.println(
