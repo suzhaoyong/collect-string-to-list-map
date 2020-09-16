444acb7ab321ef0b8e9255e7bad9f05f918829f9
biantaiwen <biantaiwen@163.com>
2020-09-16 15:42:05 +0800

    done (#165)
    done (#165)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +10,28 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Map<String, List<User>> departmentMap = new HashMap<String, List<User>>();
+        for (User user : users) {
+            List<User> userList = departmentMap.get(user.getDepartment());
+            if (userList == null) {
+                List<User> initUsers = new ArrayList<User>();
+                initUsers.add(user);
+                departmentMap.put(user.getDepartment(), initUsers);
+            } else {
+                userList.add(user);
+            }
+        }
+
+        return sortUserList(departmentMap);
+    }
+
+    public static Map<String, List<User>> sortUserList(Map<String, List<User>> departmentUserMap) {
+        for (List<User> users : departmentUserMap.values()) {
+            Collections.sort(users);
+        }
+        return departmentUserMap;
+    }
 
     public static void main(String[] args) {
         System.out.println(
