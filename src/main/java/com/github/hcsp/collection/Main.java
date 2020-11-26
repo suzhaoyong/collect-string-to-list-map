459409083db85ebec6a0157b7328aaaffb7ba01b
Ke Rui <31921365+kerrypnx@users.noreply.github.com>
2020-11-26 17:10:07 +0800

    Add a function: Mapping user by department (#172)
    Add a function: Mapping user by department (#172)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -3,6 +3,10 @@ package com.github.hcsp.collection;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +16,36 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Map<String, List<User>> departMap = new HashMap<>();
+        List<User> hasExistedDepartList = new ArrayList<User>();
+        for (User currUser : users
+        ) {
+            if (!departMap.containsKey(currUser.getDepartment().toString())) {
+                //当用户的部门不存在map的时候，将员工对象加入新的List
+                List<User> notExistDepartList = new ArrayList<User>();
+                notExistDepartList.add(currUser);
+                departMap.put(currUser.getDepartment(), notExistDepartList);
+                hasExistedDepartList = notExistDepartList;
+            } else {
+                //当用户的部门存在map的时候，将员工对象加入已经存在的List
+
+                hasExistedDepartList.add(currUser);
+                Collections.sort(hasExistedDepartList, new Comparator<User>() {
+                    @Override
+                    public int compare(User s1, User s2) {
+                        int num = s1.getAge() - s2.getAge();
+                        int num2 = num == 0 ? s1.getName().compareTo(s2.getName()) : num;
+                        return num2;
+                    }
+
+                });
+                departMap.put(currUser.getDepartment(), hasExistedDepartList);
+            }
+
+        }
+        return departMap;
+    }
 
     public static void main(String[] args) {
         System.out.println(
