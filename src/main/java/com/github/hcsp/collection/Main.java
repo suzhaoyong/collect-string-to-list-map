7c20617169225a7a8b056bb06cace3fe9ee12224
2014 <suhanma@gmail.com>
2021-02-01 08:09:40 -0800

    collect (#183)
    collect (#183)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +10,20 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Map<String, List<User>> map = new HashMap<String, List<User>>();
+        for (User user : users) {
+            List<User> list = map.get(user.getDepartment());
+            if (list == null) {
+                list = new ArrayList<User>();
+            }
+            list.add(user);
+            map.put(user.getDepartment(), list);
+        }
+
+        map.forEach((k, v) -> Collections.sort(v));
+        return map;
+    }
 
     public static void main(String[] args) {
         System.out.println(
