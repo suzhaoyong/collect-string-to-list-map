7d1202797eb7e40db16e82a803ffb157c0e5ddfd
Ludwig <392559142@qq.com>
2021-10-09 00:48:15 +0800

    fixed (#198)
    fixed (#198)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -3,6 +3,7 @@ package com.github.hcsp.collection;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +13,29 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        HashMap<String, List<User>> usersByDepartment = new HashMap<>();
+        for (User user : users) {
+            ArrayList<User> multiUser = new ArrayList<>();
+            if (usersByDepartment.containsKey(user.getDepartment())) {    // 已有key，复制原有的 value
+                multiUser.addAll(usersByDepartment.get(user.getDepartment()));
+            }
+            if (!usersByDepartment.containsValue(user)) {     // 避免重复录入
+                multiUser.add(user);
+            }
+            usersByDepartment.put(user.getDepartment(), multiUser);
+        }
+
+        for (String department : usersByDepartment.keySet()) {
+            usersByDepartment.get(department).sort(new Comparator<User>() {
+                @Override
+                public int compare(User o1, User o2) {
+                    return Integer.compare(o1.getAge(), o2.getAge());
+                }
+            });
+        }
+        return usersByDepartment;
+    }
 
     public static void main(String[] args) {
         System.out.println(
