ead41a640dac7d24cace15b02685a1350f8bbbe2
maomao <47076197+zdmhahaha@users.noreply.github.com>
2021-10-25 23:12:44 +0800

    重做 (#203)
    重做 (#203)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +10,27 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Set<String> set = new HashSet();
+        users.forEach(user -> {
+            set.add(user.getDepartment());
+        });
+        Map<String, List<User>> map = new HashMap<>();
+        // 遍历所有的部门，再遍历所有对象将是这个的对象加到这个部门
+        set.forEach(department -> {
+            ArrayList list = new ArrayList();
+            users.forEach(user -> {
+                if (user.getDepartment() == department) {
+                    list.add(user);
+                }
+            });
+
+            // 将每个部门的对象的年龄按照从小到大的顺序排序
+            Collections.sort(list, new SortByUserId());
+            map.put(department, list);
+        });
+        return map;
+    }
 
     public static void main(String[] args) {
         System.out.println(
@@ -21,5 +39,15 @@ public class Main {
                                 new User(1, "张三", 40, "技术部"),
                                 new User(2, "李四", 30, "技术部"),
                                 new User(3, "王五", 40, "市场部"))));
+    }
+
+    private static class SortByUserId implements Comparator<User> {
+        @Override
+        public int compare(User o1, User o2) {
+            if (o1.getAge() > o2.getAge()) {
+                return 1;
+            }
+            return -1;
+        }
     }
 }
