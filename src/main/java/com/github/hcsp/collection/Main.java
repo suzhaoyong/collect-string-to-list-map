ca4bc015a9537d9a43dab455960fd9cf176d7b1e
ang tsang <tsang1205@qq.com>
2022-05-29 16:09:31 +0800

    feat: collect string to list map (#224)
    feat: collect string to list map (#224)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +10,27 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Map<String, List<User>> result = new HashMap<>();
+        for (User user : users) {
+            String key = user.getDepartment();
+            List<User> userList;
+            if (result.containsKey(key)) {
+                userList = result.get(key);
+            } else {
+                userList = new ArrayList<>();
+            }
+            userList.add(user);
+            userList.sort(new Comparator<User>() {
+                @Override
+                public int compare(User o1, User o2) {
+                    return o1.getAge() - o2.getAge();
+                }
+            });
+            result.put(key, userList);
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
         System.out.println(
