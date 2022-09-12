f229c1d8624045e2b07f6487be731bfbbfdda9e0
wjxalexander <37684072+wjxalexander@users.noreply.github.com>
2022-09-12 12:24:34 +0800

    done (#233)
    done (#233)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +10,17 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        //https://stackoverflow.com/questions/48563867/add-to-list-value-on-hashmap
+        Map<String, List<User>> hashMap = new HashMap<>();
+        for (User user : users) {
+            hashMap.computeIfAbsent(user.getDepartment(), key -> new ArrayList<>()).add(user);
+        }
+        for (List<User> list : hashMap.values()) {
+            Collections.sort(list);
+        }
+        return hashMap;
+    }
 
     public static void main(String[] args) {
         System.out.println(
