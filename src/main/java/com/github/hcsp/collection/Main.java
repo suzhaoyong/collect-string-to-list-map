79e148f311d3a16e8079351cc88ce72eed4304bc
阿嚏 <47130151+A-Tione@users.noreply.github.com>
2023-08-17 16:54:57 +0800

    - [x] done (#238)
    - [x] done (#238)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,10 @@
 package com.github.hcsp.collection;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +14,11 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        List<User> ArrayList = new ArrayList<>(users);
+        ArrayList.sort((o1, o2) -> o1.getAge() - o2.getAge());
+        return ArrayList.stream().collect(Collectors.groupingBy(User::getDepartment));
+    }
 
     public static void main(String[] args) {
         System.out.println(
