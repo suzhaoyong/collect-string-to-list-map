c67b09b196371a51a18fcf2593f13575f68ee4be
qbbo <qbbo@outlook.com>
2023-12-07 20:20:01 +0800

    save user to hashmap & divide different department

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,9 +1,11 @@
 package com.github.hcsp.collection;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+import java.util.Collections;
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
     // 返回一个从部门名到这个部门的所有用户的映射。同一个部门的用户按照年龄进行从小到大排序。
@@ -12,7 +14,22 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Map<String, List<User>> map = new HashMap<>();
+        for (User user : users) {
+            if (map.containsKey(user.getDepartment())) {
+                List<User> departmentUsers = map.get(user.getDepartment());
+                departmentUsers.add(user);
+                Collections.sort(departmentUsers);
+                map.put(user.getDepartment(), departmentUsers);
+                continue;
+            }
+            List<User> initDepartmentUser = new ArrayList<>();
+            initDepartmentUser.add(user);
+            map.put(user.getDepartment(), initDepartmentUser);
+        }
+        return map;
+    }
 
     public static void main(String[] args) {
         System.out.println(
